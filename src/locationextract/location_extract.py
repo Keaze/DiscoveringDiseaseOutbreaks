4981739caf057b9c3b90d9f65f15b5adf386fa15
Daniel <danielwiese@sent.com>
2021-02-07 01:00:53 +0100

    Refactoring location_extract.py
    Class for Location Extract to eliminate the need to recompile the regex every time

--- src/locationextract/location_extract.py
+++ src/locationextract/location_extract.py
@@ -1,5 +1,39 @@
-import geonamescache
 import re
+
+import geonamescache
+
+
+class LocationFinder():
+    def __init__(self, regex):
+        self.regex = regex
+
+    def find(self, string):
+        s = re.finditer(self.regex, string)
+        return list(x.group() for x in s)
+
+
+def create_city_finder():
+    result = list(
+        [x["name"] for x in geonamescache.GeonamesCache().get_cities().values()]
+    )
+    regex_compiled = _compile_regex(result)
+    return LocationFinder(regex_compiled)
+
+
+def create_country_finder():
+    result = list(
+        [x["name"] for x in geonamescache.GeonamesCache().get_countries().values()]
+    )
+    regex_compiled = _compile_regex(result)
+    return LocationFinder(regex_compiled)
+
+
+def _compile_regex(result):
+    distinct_values = list(set(result))
+    distinct_values.sort(key=len, reverse=True)
+    regex = r"\b" + r"\b|\b".join(distinct_values) + r"\b"
+    regex_compiled = re.compile(regex)
+    return regex_compiled
 
 
 def find_countries(string):
@@ -7,14 +41,7 @@ def find_countries(string):
     Returns the countries listed in an input string
     :return: List of countries in the input string
     """
-    result = list(
-        [x["name"] for x in geonamescache.GeonamesCache().get_countries().values()]
-    )
-    distinct_countries = list(set(result))
-    regex = "|".join(distinct_countries)
-    regex_compiled = re.compile(regex)
-    s = re.finditer(regex_compiled, string)
-    return list(x.group() for x in s)
+    return create_country_finder().find(string)
 
 
 def extract_locations(string):
@@ -29,14 +56,7 @@ def find_cities(string):
     Returns the cities listed in an input string
     :return: List of cities in the input string
     """
-    result = list(
-        [x["name"] for x in geonamescache.GeonamesCache().get_cities().values()]
-    )
-    distinct_cities = list(set(result))
-    regex = "|".join(distinct_cities)
-    regex_compiled = re.compile(regex)
-    s = re.finditer(regex_compiled, string)
-    return list(x.group() for x in s)
+    return create_city_finder().find(string)
 
 
 if __name__ == "__main__":
--- tests/test_location_extract.py
+++ tests/test_location_extract.py
@@ -1,45 +1,68 @@
-from locationextract.location_extract import extract_locations
+from locationextract.location_extract import (
+    extract_locations,
+    find_cities,
+    find_countries,
+)
 
 
 def test_find_city():
-    result = extract_locations("Berlin")
+    result = find_cities("Berlin")
     assert "Berlin" in result
 
+
 def test_find_city_with_two_words():
-    result = extract_locations("San Francisco")
+    result = find_cities("San Francisco")
     assert "San Francisco" in result
 
+
 def test_find_another_city():
-    result = extract_locations("Vienna")
+    result = find_cities("Vienna")
     assert "Vienna" in result
 
 
 def test_find_city_in_string():
-    result = extract_locations("The capital of this nation is Berlin")
+    result = find_cities("The capital of this nation is Berlin")
     assert "Berlin" in result
 
 
 def test_find_multiple_cities_in_string():
-    result = extract_locations("Paris and Berlin and Vienna")
+    result = find_cities("Paris and Berlin and Vienna")
     assert "Berlin" in result
     assert "Paris" in result
     assert "Vienna" in result
 
 
 def test_find_country():
-    result = extract_locations("Germany")
+    result = find_countries("Germany")
     assert "Germany" in result
 
 
 def test_find_multiple_countries():
-    result = extract_locations("Germany and Austria")
+    result = find_countries("Germany and Austria")
     assert "Germany" in result
     assert "Austria" in result
 
-def test_with_headlin():
-    result = extract_locations("Flu outbreak in Galveston, Texas")
+
+def test_with_headline():
+    result = find_cities("Flu outbreak in Galveston, Texas")
     assert "Galveston" in result
+
 
 def test_empty_input():
     result = extract_locations("")
     assert len(result) == 0
+
+
+def test_dont_find_city_as_nation():
+    result = find_countries("Berlin")
+    assert "Berlin" not in result
+
+
+def test_dont_find_nation_as_city():
+    result = find_cities("Germany")
+    assert "Germany" not in result
+
+
+def test_part_of_word_is_a_city():
+    result = find_cities("Berlinasd")
+    assert "Berlin" not in result
